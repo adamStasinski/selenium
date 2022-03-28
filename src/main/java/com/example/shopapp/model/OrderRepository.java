17e3d73e8aa24abcd310ef8512d506d7024988f6
Adam Stasiński <adamsta@agora.pl>
2022-03-28 21:34:12 +0200

    added methods to OrderRepository for not allow delete orders

--- src/main/java/com/example/shopapp/model/OrderRepository.java
+++ src/main/java/com/example/shopapp/model/OrderRepository.java
@@ -2,8 +2,16 @@ package com.example.shopapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 interface OrderRepository extends JpaRepository<Order, Integer> {
 
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Order order);
 }
